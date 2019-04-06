29c0d4d123df9fd57cac683dbd34124d0d43294f
Muniswami <santhosh.muniswami@dxc.com>
2019-04-06 08:42:54 +0530

    latest changes

--- app.js
+++ app.js
@@ -163,27 +163,6 @@ app.get('/*', function(req, res) {
   res.end();
 });
 
-/*
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-*/
-/*
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-*/
-
 app.listen(port, host);
 console.log('App started on port ' + port);
 
